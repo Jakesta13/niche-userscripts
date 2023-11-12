cb66cf83630c986a6accdc8ac747abac7ca21cdb
Jake <10874330+Jakesta13@users.noreply.github.com>
2023-11-12 16:42:27 -0500

    Added another userscript for amazon.ca

---
+++ Amazon/amazon-buyagain-coupon-clipper.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Amazon BuyAgain Clip Coupons
+// @namespace    ChatGPT
+// @version      0.2
+// @description  Clicks on checkboxes on the Amazon Buy Again page with a specific aria-label pattern (coupons)
+// @author       ChatGPT
+// @match        https://www.amazon.ca/gp/buyagain/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to click on checkboxes with a specific aria-label pattern
+    function clickCheckboxes() {
+        var checkboxes = document.querySelectorAll('input[type="checkbox"][aria-label^="Save $"]');
+        checkboxes.forEach(function(checkbox) {
+            checkbox.click();
+        });
+    }
+
+    // Callback function for the mutation observer
+    function mutationCallback(mutationsList, observer) {
+        for (var mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                clickCheckboxes();
+            }
+        }
+    }
+
+    // Create a mutation observer to watch for changes in the DOM
+    var observer = new MutationObserver(mutationCallback);
+
+    // Options for the observer (watch for changes in child elements)
+    var observerConfig = { childList: true, subtree: true };
+
+    // Start observing the target node for mutations
+    observer.observe(document.body, observerConfig);
+
+    // Initial check when the page has finished loading
+    window.addEventListener('load', function() {
+        clickCheckboxes();
+    });
+})();
