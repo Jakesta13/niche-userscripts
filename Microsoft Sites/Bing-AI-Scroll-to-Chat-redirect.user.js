55a55e807f99e3ef412ea8cfd6db92ee80e0ffd3
Jake <10874330+Jakesta13@users.noreply.github.com>
2024-02-24 14:02:56 -0500

    added download url to make tampermonkey happy

--- Microsoft Sites/Bing-AI-Scroll-to-Chat-redirect.user.js
+++ Microsoft Sites/Bing-AI-Scroll-to-Chat-redirect.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Say No to Bing AI scroll-to-chat
 // @namespace    ChatGPT
-// @version      1
+// @version      1.0a
 // @description  Change bing's AI scroll-toggled chat is quickly navigated away from, to asist with disabling the AI if another script fails, it makes sure that &showconv= is set to "0" and update it immediately if it changes to "1"
 // @author       ChatGPT / jakesta13
 // @match        https://www.bing.com/*
 // @grant        none
 // @updateURL    https://github.com/Jakesta13/niche-userscripts/raw/main/Microsoft%20Sites/Bing-AI-Scroll-to-Chat-redirect.user.js
+// @downloadURL  https://github.com/Jakesta13/niche-userscripts/raw/main/Microsoft%20Sites/Bing-AI-Scroll-to-Chat-redirect.user.js
 // ==/UserScript==
 
 (function() {
