db561e0ff748321b3d631ad31431519a5fade70f
Jake <10874330+Jakesta13@users.noreply.github.com>
2024-12-13 16:21:13 -0500

    updated version number because i forgor

--- Microsoft Sites/Remove-Bing-Chat-AI.user.js
+++ Microsoft Sites/Remove-Bing-Chat-AI.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Bing Chat AI
 // @namespace    ChatGPT / jakesta13
-// @version      2.0
+// @version      2.1
 // @description  Remove Bing Chat AI, because we don't like being forced to use it.
 // @author       ChatGPT / jakesta13
 // @match        *://www.bing.com/*
