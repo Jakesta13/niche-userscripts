adb75f9691b8cc3ce8f72006e7e3bc49e6bbac1e
Jake <10874330+Jakesta13@users.noreply.github.com>
2025-01-05 12:04:25 -0500

    Feedback Button + Added element to remove, the button showed up under a different ID on the videos search section

--- Microsoft Sites/Remove-Bing-Chat-AI.user.js
+++ Microsoft Sites/Remove-Bing-Chat-AI.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Bing Chat AI
 // @namespace    ChatGPT / jakesta13
-// @version      3.1
+// @version      3.2
 // @description  Remove Bing Chat AI, because we don't like being forced to use it.
 // @author       ChatGPT / jakesta13
 // @match        *://www.bing.com/*
@@ -182,6 +182,7 @@
                 // - // - // - //
                 // Removes the Feedback button
                 removeElementById('sb_feedback');
+                removeElementById('fbpgbt');
                 //
                 //// - //// - ////
                 // Remove elements by class name
